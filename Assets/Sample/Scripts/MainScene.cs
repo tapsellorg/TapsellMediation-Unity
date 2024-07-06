0407e5d348039c8c5f3e574700bd1addc7043c95
Morteza Nedaei <nedaei1375@gmail.com>
2024-07-06 17:36:57 +0330

    fix: send user consent once only when the main scene is loaded

---
+++ Assets/Sample/Scripts/MainScene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class MainScene : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void OnRuntimeInitialize()
+    {
+        // send result of user consent dialog to Tapsell.
+        SetUserConsent(); 
+    }
+
+    private static void SetUserConsent()
+    {
+        Tapsell.Mediation.Tapsell.SetUserConsent(true);
+    }
+}
